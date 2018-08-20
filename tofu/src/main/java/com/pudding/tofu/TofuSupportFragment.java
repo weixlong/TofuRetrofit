9858bf507a703f0a6c9d60238e943cecb01b444a
wxl <www.632716169@qq.com>
2018-08-20 14:38:43 +0800

    support

--- tofu/src/main/java/com/pudding/tofu/TofuSupportFragment.java
+++ tofu/src/main/java/com/pudding/tofu/TofuSupportFragment.java
@@ -2,11 +2,15 @@ package com.pudding.tofu;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.pudding.tofu.model.Tofu;
 
 
 import indi.yume.tools.fragmentmanager.BaseManagerFragment;
+import indi.yume.tools.fragmentmanager.SwipeBackUtil;
 
 /**
  * Created by wxl on 2018/8/20 0020.
@@ -22,4 +26,10 @@ public abstract class TofuSupportFragment extends BaseManagerFragment{
         // SwipeBackUtil.enableSwipeBackAtActivity(this); 侧滑返回
 
     }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_1,container,false);
+//        return SwipeBackUtil.enableSwipeBackAtFragment(this,view);侧滑返回
+//    }
 }
